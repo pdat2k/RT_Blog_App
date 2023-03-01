74d4b44300744bfbc5f3747ba5fb2bd79a9939f3
pdat2000 <phamdat2982k@gmail.com>
2023-03-01 18:12:52 +0700

    fix

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -4,20 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
     use HasFactory;
-
-    public function blogs(): BelongsTo
-    {
-        return $this->belongsTo(Blog::class, 'blog_id', 'id');
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class, 'user_id', 'id');
-    }
 }
