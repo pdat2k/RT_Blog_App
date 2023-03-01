5709939a6f684379060d699ab9caef6b56f1b062
pdat2000 <phamdat2982k@gmail.com>
2023-03-01 18:12:52 +0700

    fix

--- app/Models/blog.php
+++ app/Models/blog.php
@@ -4,31 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Blog extends Model
 {
     use HasFactory;
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class, 'category_id', 'id');
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class, 'user_id', 'id');
-    }
-
-    public function comments(): HasMany
-    {
-        return $this->hasMany(Comment::class, 'blog_id', 'id');
-    }
-
-    public function like(): BelongsTo
-    {
-        return $this->belongsTo(Like::class, 'blog_id', 'id');
-    }
 }
