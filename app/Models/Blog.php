b24e2bafaf747a6d9453ae1a755698d2abf35357
pdat2000 <phamdat2982k@gmail.com>
2023-03-02 09:08:17 +0700

    feature_migration

---
+++ app/Models/Blog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Blog extends Model
+{
+    use HasFactory;
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'blog_id', 'id');
+    }
+    public function like(): BelongsTo
+    {
+        return $this->belongsTo(Like::class, 'blog_id', 'id');
+    }
+}
