4f9ebc1d8c993c9886697302a85e8eed2af93de7
pdat2000 <phamdat2982k@gmail.com>
2023-03-08 18:12:57 +0700

    service

---
+++ resources/views/auth/forget.blade.php
@@ -0,0 +1,20 @@
+@extends('auth.app')
+
+@section('title','Forgot')
+
+@section('name','Forgot')
+
+@section('container')
+    <form action="" method="POST" autocomplete="off" >
+        <x-form.input>
+            <x-slot name='label'>Email</x-slot>
+            <x-slot name='type'>email</x-slot>
+            <x-slot name='input'>email</x-slot>
+        </x-form.input>
+        <x-form.button>
+            <x-slot name='button'>Send</x-slot>
+            <x-slot name='href'>auth.login</x-slot>
+            <x-slot name='link'>Already have an account? Login</x-slot>
+        </x-form.button>
+    </form>
+@endsection
---
+++ resources/views/layouts/create.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'Create blog')
+
+@section('content')
+        @include('layouts.header')
+        @include('layouts.aside')
+        @include('layouts.footer')
+@endsection
